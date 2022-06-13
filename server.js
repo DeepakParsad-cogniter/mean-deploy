1ecba00c820920d2b66a1cfe1385daa14f0f25e0
deepak <dparsad@cogniter.com>
2022-06-13 11:40:05 +0530

    driver changed (from mongoose to mongoclient)

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 const tweetRoute = require("./routes/tweet");
 
 require("dotenv").config();
@@ -17,10 +18,11 @@ app.use("/tweets", tweetRoute);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }, (err) => {
-    if (err) console.error(err);
-});
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb+srv://dparsad:@password123@cluster0.mqasazh.mongodb.net/?retryWrites=true&w=majority", { useMongoClient: true }, (err) => {
+//     if (err) console.error(err);
+// });
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
